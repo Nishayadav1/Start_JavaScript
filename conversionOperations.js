ca6bbcc2a2a5fad9e53a5699c4bbbaf35d824d77
Nishayadav1 <nishayadav22@navgurukul.org>
2024-04-10 15:31:30 +0530

    conversion operation

---
+++ conversionOperations.js
@@ -0,0 +1,38 @@
+let someNumber=33;
+
+let stringNumber= String(someNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// *********************************operations*****************************************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let Name="nisha"
+// let srName="yadav"
+//  console.log(Name + srName)
+
+
+// //  to perimitive
+//  console.log("1"+2) //12
+//  console.log(1+"2") //12
+//  console.log("1"+"2") //12
+//  console.log("1"+2+2) //122
+//  console.log(1+2+"2") //32
+
+//  console.log(+true) //1
+//  console.log(+"")   //0
+
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter)
+
+// prefix ++gameCounter
+// profix gameCounter++
