3909bd812bcebd87bc4fd8dc83b78bd60b36717d
Nishayadav1 <nishayadav22@navgurukul.org>
2024-04-10 15:41:35 +0530

    comparison

---
+++ comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// typescript    ?????????????????????????
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined<0)s
+
+
+// === stric check
+console.log("2"===2) // check both (type and value)
