a8854486c462e1f854d9d4bd7f4ccf8b2e7fb124
Nishayadav1 <nishayadav22@navgurukul.org>
2024-04-10 16:22:14 +0530

    summary od data types

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,59 @@
+// primitive 
+//----- 7 type : string, Number, Boolean, Null, Undifine, Bigint, symbol
+
+
+const score=100
+console.log(typeof score)
+
+const scroeValue=100.3
+console.log(typeof scroeValue)
+
+const isLoggeIn=false
+console.log(typeof isLoggeIn)
+
+const outsideTemp=null
+console.log(typeof outsideTemp)
+
+let userEmail;
+console.log(typeof userEmail)
+
+const id =Symbol('123')
+console.log(typeof id)
+
+const anotherId=Symbol('123')
+console.log(typeof anotherId)
+
+console.log(id===anotherId)
+
+
+const bigNumber= 12345764325789887656789n
+console.log(typeof bigNumber)
+
+
+
+
+
+// Reference   (non primitive)
+// Aarray, Object, Function
+
+// const animals=["dog", "cat", "cow"] 
+// ---Array
+
+// let object={
+//     name:"nisha",
+//     lastname:"yadav"
+// }          
+//  ------object
+
+
+
+// const myFunction= function(){
+//     console.log("hello world")
+// }
+// ---function
+
+
+
+
+// JavaScript is a dynamically-typed language
+// typescript ?????????????????????????????????????????? 
