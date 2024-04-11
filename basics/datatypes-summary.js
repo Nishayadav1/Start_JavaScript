5b46472449360d0f0799ba9e8e905988a1889e3f
Nishayadav1 <nishayadav22@navgurukul.org>
2024-04-11 15:56:04 +0530

    stack and Heap memory

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -2,32 +2,32 @@
 //----- 7 type : string, Number, Boolean, Null, Undifine, Bigint, symbol
 
 
-const score=100
-console.log(typeof score)
+// const score=100
+// console.log(typeof score)
 
-const scroeValue=100.3
-console.log(typeof scroeValue)
+// const scroeValue=100.3
+// console.log(typeof scroeValue)
 
-const isLoggeIn=false
-console.log(typeof isLoggeIn)
+// const isLoggeIn=false
+// console.log(typeof isLoggeIn)
 
-const outsideTemp=null
-console.log(typeof outsideTemp)
+// const outsideTemp=null
+// console.log(typeof outsideTemp)
 
-let userEmail;
-console.log(typeof userEmail)
+// let userEmail;
+// console.log(typeof userEmail)
 
-const id =Symbol('123')
-console.log(typeof id)
+// const id =Symbol('123')
+// console.log(typeof id)
 
-const anotherId=Symbol('123')
-console.log(typeof anotherId)
+// const anotherId=Symbol('123')
+// console.log(typeof anotherId)
 
-console.log(id===anotherId)
+// console.log(id===anotherId)
 
 
-const bigNumber= 12345764325789887656789n
-console.log(typeof bigNumber)
+// const bigNumber= 12345764325789887656789n
+// console.log(typeof bigNumber)
 
 
 
@@ -56,4 +56,29 @@ console.log(typeof bigNumber)
 
 
 // JavaScript is a dynamically-typed language
-// typescript ?????????????????????????????????????????? 
+// typescript ??????????????????????????????????????????
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// how to work meomeory
+
+// stack memory(primitive)----change in copy
+let myName="nisha"
+
+let anotherName= myName
+anotherName= "komal"
+
+console.log(myName)
+console.log(anotherName)
+
+// Heap (non primitive)------origan value change , get refrence by heap
+let useOne={
+    email:"nisha@gmail.com",
+    upi: "userone@kl"
+}
+
+let userTwo=useOne
+userTwo.email="kaljal@.com"
+console.log(useOne.email)
+console.log(userTwo.email)
