82261ee45750f8a8b61cf336b5512ea8ba41054b
Nishayadav1 <nishayadav22@navgurukul.org>
2024-04-12 15:23:11 +0530

    number and math

---
+++ basics/numbersanMath.js
@@ -0,0 +1,31 @@
+// const score=400 //difine in number
+
+// const balance= new Number(100); //difine in object
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //after demimal digit
+
+// const otherNumber=23.8966
+// console.log(otherNumber.toPrecision(2)) //round of accoding  number
+
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-In')); //work on comma (In)--point (en)-comma
+
+// -------------------------------MAth----------------------------
+
+
+// console.log(Math.abs(-4)) //-value convert in posiitive
+// console.log(Math.round(4.9))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(8.9))
+// console.log(Math.min(2,4,7,1,4))
+// console.log(Math.max(2,4,7,1,4))
+// console.log((Math.random()*10)+1)
+
+// ------------------------------------random--------------------------------------------
+const min=10
+const max=20
+console.log((Math.floor (Math.random()* (max-min+1) +min))+min)
