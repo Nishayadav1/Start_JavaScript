de799a5044fcf15d75a1d85dd5c5da9b6ef81413
Nishayadav1 <nishayadav22@navgurukul.org>
2024-04-21 16:05:24 +0530

    Array and method

---
+++ 02_basic/01_array.js
@@ -0,0 +1,42 @@
+// array
+
+const myArray=[1,2,3,5,,6,879,253,568,]
+const myHeros=["papa", "mummy"]
+
+const myArr2= new Array(1,2,3,4)
+// console.log(myArr2[2]);
+
+//Array method
+
+// myArr2.push(6)
+// myArr2.push(7)
+// console.log(myArr2)
+
+// myArr2.pop()
+// console.log(myArr2)
+
+// myArr2.unshift(9)
+// myArr2.shift()
+// console.log(myArr2)
+
+// console.log(myArr2.includes(9));
+// console.log(myArr2.includes(3));
+
+
+
+// const Arr2 =myArr2.join
+// console.log(Arr2)
+// console.log(myArr2)
+
+
+//slice and splice
+
+console.log("A",myArr2);
+
+const l=myArr2.slice(1,3)
+console.log(l)
+
+console.log("B",myArr2);
+const l1=myArr2.splice(3) ///remover 3 element
+console.log(l1)
+
