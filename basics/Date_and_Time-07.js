b70e6c9cae91bc3c44f4a238ecbaa5704ee5f612
Nishayadav1 <nishayadav22@navgurukul.org>
2024-04-21 15:42:29 +0530

    date and time

---
+++ basics/Date_and_Time-07.js
@@ -0,0 +1,33 @@
+// let myDate= new Date()
+// console.log(typeof(myDate))
+
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toTimeString())
+
+// let myCreatedDate= new Date(2024,11,10)
+// console.log(myCreatedDate.toString())
+// console.log(myCreatedDate.toLocaleString()) 
+
+let myCreatedDate= new Date("01-11-2024")
+// console.log(myCreatedDate.toLocaleString())
+// console.log(myCreatedDate.toLocaleDateString())
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp); //in milli sec.
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+
+// get----------------------------------------------------------
+let date= new Date()
+console.log(date.getTime())
+console.log(date.getDate())
+console.log(date.getMonth()+1)
+console.log(date.getFullYear())
+
+// newDate.toLocalString('defualt'),{
+//     weekday:"long"
+// }******************************************doubt
